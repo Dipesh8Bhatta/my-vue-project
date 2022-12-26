4e647314e5d973630bf93d370508fbe000f06e10
dipeshb <dipeshb@ibsglobalweb.com>
2022-12-26 13:58:02 +1100

    DB: Add data and link to route

--- src/pages/ProductListPage.vue
+++ src/pages/ProductListPage.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>Product List Page</h1>
+  <div>
+    <h2>Available Items:</h2>
+    <div v-for="product in products" :key="product.id">
+      <router-link :to="`/product/${product.id}`">
+        <h3>{{ product.name }}</h3>
+      </router-link>
+      <h3>{{ product.price }}</h3>
+      <button>Add to Cart</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import products from "./products";
 export default {
   name: 'ProductListPage',
+  data() {
+    return {
+      products,
+    }
+  },
 };
 </script>
---
+++ src/pages/products.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: '123',
+    name: 'Bag of Potato chips',
+    price: '$4.99',
+    description: 'Extra salty, and quity tasty'
+  },
+  {
+    id: '234',
+    name: 'Ketchup',
+    price: '$2.99',
+    description: 'Made from fresh tomatoes'
+  },
+  {
+    id: '355',
+    name: 'Hamburgers',
+    price: '$7.99',
+    description: 'Made from 100% Aussie food'
+  }
+  ]
